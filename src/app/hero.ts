94d251603487d3419a1b8b8a88da9b4ea1ef93b3
mrima <mrimamss@gmail.com>
2025-04-25 08:59:08 +0300

    first commit

---
+++ src/app/hero.ts
@@ -0,0 +1,16 @@
+// hero.ts
+import { heroui } from "@heroui/react";
+
+export default heroui({
+      themes: {
+        dark: {
+          colors: {
+            primary: {
+              DEFAULT: "#BEF264",
+              foreground: "#000000",
+            },
+            focus: "#BEF264",
+          },
+        },
+      },
+    });
---
+++ theme.js
@@ -0,0 +1,113 @@
+export const lightTheme = {
+	type: "light",
+	theme: {
+		colors: {
+			default: {
+				50: "#fafafa",
+				100: "#f2f2f3",
+				200: "#ebebec",
+				300: "#e3e3e6",
+				400: "#dcdcdf",
+				500: "#d4d4d8",
+				600: "#afafb2",
+				700: "#8a8a8c",
+				800: "#656567",
+				900: "#404041",
+				foreground: "#000",
+				DEFAULT: "#d4d4d8",
+			},
+			primary: {
+				50: "#fddfdf",
+				100: "#fab3b3",
+				200: "#f68686",
+				300: "#f35959",
+				400: "#f02d2d",
+				500: "#ed0000",
+				600: "#c40000",
+				700: "#9a0000",
+				800: "#710000",
+				900: "#470000",
+				foreground: "#000",
+				DEFAULT: "#ed0000",
+			},
+			// Add other color sections (secondary, success, etc.)
+			background: "#ffffff",
+			foreground: "#000000",
+			focus: "#006FEE",
+			overlay: "#000000",
+			content1: {
+				DEFAULT: "#ffffff",
+				foreground: "#000",
+			},
+			content2: {
+				DEFAULT: "#f4f4f5",
+				foreground: "#000",
+			},
+			content3: {
+				DEFAULT: "#e4e4e7",
+				foreground: "#000",
+			},
+			content4: {
+				DEFAULT: "#d4d4d8",
+				foreground: "#000",
+			},
+		},
+	},
+};
+
+export const darkTheme = {
+	type: "dark",
+	theme: {
+		colors: {
+			default: {
+				50: "#2f0303",
+				100: "#5f0505",
+				200: "#8e0808",
+				300: "#be0a0a",
+				400: "#ed0d0d",
+				500: "#f13d3d",
+				600: "#f46e6e",
+				700: "#f89e9e",
+				800: "#fbcfcf",
+				900: "#ffffff",
+				foreground: "#000",
+				DEFAULT: "#ed0d0d",
+			},
+			primary: {
+				50: "#470000",
+				100: "#710000",
+				200: "#9a0000",
+				300: "#c40000",
+				400: "#ed0000",
+				500: "#f02d2d",
+				600: "#f35959",
+				700: "#f68686",
+				800: "#fab3b3",
+				900: "#fddfdf",
+				foreground: "#000",
+				DEFAULT: "#ed0000",
+			},
+			// Add the rest like above
+			background: "#000000",
+			foreground: "#ffffff",
+			focus: "#006FEE",
+			overlay: "#ffffff",
+			content1: {
+				DEFAULT: "#18181b",
+				foreground: "#fff",
+			},
+			content2: {
+				DEFAULT: "#27272a",
+				foreground: "#fff",
+			},
+			content3: {
+				DEFAULT: "#3f3f46",
+				foreground: "#fff",
+			},
+			content4: {
+				DEFAULT: "#52525b",
+				foreground: "#fff",
+			},
+		},
+	},
+};
